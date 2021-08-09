ea2c0e3448f2af6bc489da99980ea371a10fde35
ShakibAhmed007 <shakibahmed09112017@gmail.com>
2021-08-09 14:01:28 +0600

    1 file modified, 3 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import ClickCounter from './components/ClickCounter';
+import HoverCounter from './components/HoverCounter';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Higher Order Component</h1>
+      <p> Similar Functional Component should be reused </p>
+      <ClickCounter />
+      <HoverCounter />
     </div>
   );
 }
